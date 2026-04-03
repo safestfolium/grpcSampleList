b377be4b0aba0db9cc53c63e396c33d2dcc3a944
Walker Jacob <mgarc159@estudiante.ibero.edu.co>
2026-04-03 16:19:26 +0800

    Update Hello.cs with new content

--- GrpcShared/Generated/Hello.cs
+++ GrpcShared/Generated/Hello.cs
@@ -1,19 +1,9 @@
-using System;
 using Google.Protobuf;
-using Google.Protobuf.Reflection;
 
 namespace GrpcShared.Generated
 {
-    public sealed class HelloRequest : IMessage<HelloRequest>
+    public sealed class HelloRequest
     {
-        private static readonly MessageParser<HelloRequest> _parser = new MessageParser<HelloRequest>(() => new HelloRequest());
-
-        public static MessageParser<HelloRequest> Parser { get { return _parser; } }
-
-        public static MessageDescriptor Descriptor { get { return null; } }
-
-        MessageDescriptor IMessage.Descriptor { get { return Descriptor; } }
-
         private string _name = string.Empty;
 
         public string Name
@@ -23,39 +13,6 @@ namespace GrpcShared.Generated
         }
 
         public HelloRequest() { }
-
-        public HelloRequest(HelloRequest other)
-        {
-            _name = other._name;
-        }
-
-        public HelloRequest Clone()
-        {
-            return new HelloRequest(this);
-        }
-
-        public void MergeFrom(HelloRequest other)
-        {
-            if (other == null) return;
-            if (other._name.Length != 0) _name = other._name;
-        }
-
-        public void MergeFrom(CodedInputStream input)
-        {
-            uint tag;
-            while ((tag = input.ReadTag()) != 0)
-            {
-                switch (tag)
-                {
-                    case 10:
-                        Name = input.ReadString();
-                        break;
-                    default:
-                        input.SkipLastField();
-                        break;
-                }
-            }
-        }
 
         public void WriteTo(CodedOutputStream output)
         {
@@ -70,34 +27,8 @@ namespace GrpcShared.Generated
         {
             int size = 0;
             if (_name.Length != 0)
-            {
                 size += 1 + CodedOutputStream.ComputeStringSize(_name);
-            }
             return size;
-        }
-
-        public bool Equals(HelloRequest other)
-        {
-            if (ReferenceEquals(other, null)) return false;
-            if (ReferenceEquals(other, this)) return true;
-            return _name == other._name;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as HelloRequest);
-        }
-
-        public override int GetHashCode()
-        {
-            int hash = 1;
-            if (_name.Length != 0) hash ^= _name.GetHashCode();
-            return hash;
-        }
-
-        public override string ToString()
-        {
-            return string.Format("HelloRequest {{ Name = {0} }}", _name);
         }
 
         public byte[] ToByteArray()
@@ -108,18 +39,30 @@ namespace GrpcShared.Generated
             output.Flush();
             return result;
         }
+
+        public static HelloRequest ParseFrom(byte[] data)
+        {
+            HelloRequest msg = new HelloRequest();
+            CodedInputStream input = new CodedInputStream(data);
+            uint tag;
+            while ((tag = input.ReadTag()) != 0)
+            {
+                switch (tag)
+                {
+                    case 10:
+                        msg.Name = input.ReadString();
+                        break;
+                    default:
+                        input.SkipLastField();
+                        break;
+                }
+            }
+            return msg;
+        }
     }
 
-    public sealed class HelloReply : IMessage<HelloReply>
+    public sealed class HelloReply
     {
-        private static readonly MessageParser<HelloReply> _parser = new MessageParser<HelloReply>(() => new HelloReply());
-
-        public static MessageParser<HelloReply> Parser { get { return _parser; } }
-
-        public static MessageDescriptor Descriptor { get { return null; } }
-
-        MessageDescriptor IMessage.Descriptor { get { return Descriptor; } }
-
         private string _message = string.Empty;
 
         public string Message
@@ -129,39 +72,6 @@ namespace GrpcShared.Generated
         }
 
         public HelloReply() { }
-
-        public HelloReply(HelloReply other)
-        {
-            _message = other._message;
-        }
-
-        public HelloReply Clone()
-        {
-            return new HelloReply(this);
-        }
-
-        public void MergeFrom(HelloReply other)
-        {
-            if (other == null) return;
-            if (other._message.Length != 0) _message = other._message;
-        }
-
-        public void MergeFrom(CodedInputStream input)
-        {
-            uint tag;
-            while ((tag = input.ReadTag()) != 0)
-            {
-                switch (tag)
-                {
-                    case 10:
-                        Message = input.ReadString();
-                        break;
-                    default:
-                        input.SkipLastField();
-                        break;
-                }
-            }
-        }
 
         public void WriteTo(CodedOutputStream output)
         {
@@ -176,34 +86,8 @@ namespace GrpcShared.Generated
         {
             int size = 0;
             if (_message.Length != 0)
-            {
                 size += 1 + CodedOutputStream.ComputeStringSize(_message);
-            }
             return size;
-        }
-
-        public bool Equals(HelloReply other)
-        {
-            if (ReferenceEquals(other, null)) return false;
-            if (ReferenceEquals(other, this)) return true;
-            return _message == other._message;
-        }
-
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as HelloReply);
-        }
-
-        public override int GetHashCode()
-        {
-            int hash = 1;
-            if (_message.Length != 0) hash ^= _message.GetHashCode();
-            return hash;
-        }
-
-        public override string ToString()
-        {
-            return string.Format("HelloReply {{ Message = {0} }}", _message);
         }
 
         public byte[] ToByteArray()
@@ -213,6 +97,26 @@ namespace GrpcShared.Generated
             WriteTo(output);
             output.Flush();
             return result;
+        }
+
+        public static HelloReply ParseFrom(byte[] data)
+        {
+            HelloReply msg = new HelloReply();
+            CodedInputStream input = new CodedInputStream(data);
+            uint tag;
+            while ((tag = input.ReadTag()) != 0)
+            {
+                switch (tag)
+                {
+                    case 10:
+                        msg.Message = input.ReadString();
+                        break;
+                    default:
+                        input.SkipLastField();
+                        break;
+                }
+            }
+            return msg;
         }
     }
 }
